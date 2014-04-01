ce4bec4e5a80ad0b906cd9012605a98fe2b4b241
Pham Mikun <pmquan93@yahoo.com>
2014-04-01 11:57:46 +0700

    parallax action without function

---
+++ js/parallax.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$(window).bind('scroll', function() {
+		parallax();
+	});
+});
+
+function parallax() {
+	var scrollPos = $(window).scrollTop();
+	// Section 1
+	$('.section-1').css('backgroundPosition', "50% " + Math.round(($('.section-1').offset().top - scrollPos) * 0.5) + "px");
+	// Section 2
+	$('.section-2').css('backgroundPosition', "50% " + Math.round(($('.section-2').offset().top - scrollPos) * 0.3) + "px");
+	$('.sherlock').css('top',(500 - (scrollPos * 0.5)) + 'px')
+	$('#sherlock-img').css('top',(250 - (scrollPos * 0.3)) + 'px')
+	// Section 3
+	$('.section-3').css('backgroundPosition', "50% " + Math.round(($('.section-3').offset().top - scrollPos) * 0.4) + "px");
+	$('.steve').css('top',(600 - (scrollPos * 0.5)) + 'px')
+	$('#steve-img').css('top',(600 - (scrollPos * 0.7)) + 'px')
+}
